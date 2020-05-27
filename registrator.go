08559585854a1dbe51af99e3de06a3a079a0d65f
kaizer <kaizer@kai-zer.ru>
2020-05-27 13:06:03 +0800

    add CommandStruct to Registration Register without timeout and goroutine

--- registrator.go
+++ registrator.go
@@ -6,39 +6,32 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"time"
 )
 
 type DaemonData struct {
 	ServerAddress string
 	Daemon        struct {
-		DaemonName    string `json:"name"`
-		DaemonAddress string `json:"address"`
-		Description   string `json:"description"`
+		DaemonName    string        `json:"name"`
+		DaemonAddress string        `json:"address"`
+		Description   string        `json:"description"`
+		Commands      CommandStruct `json:"commands"`
 	}
 }
 
-func (d *DaemonData) Register() {
-	go func() {
-		time.Sleep(5 * time.Second)
-		data, err := json.Marshal(d.Daemon)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println(string(data))
-		resp, err := http.Post(d.ServerAddress+"/registerService", "application/json", bytes.NewBuffer(data))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		responseData, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println(string(responseData))
-		_ = resp.Body.Close()
-		return
-	}()
+func (d *DaemonData) Register() error {
+	data, err := json.Marshal(d.Daemon)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(data))
+	resp, err := http.Post(d.ServerAddress+"/registerService", "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(responseData))
+	return resp.Body.Close()
 }
