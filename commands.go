32449fe1aa2f631156e0f22dec539ff14b33c03a
kaizer <kaizer@kai-zer.ru>
2020-05-27 12:20:44 +0800

    add required_params to CommandStruct

--- commands.go
+++ commands.go
@@ -1,8 +1,9 @@
 package serviceCommunicatorServer
 
 type CommandStruct struct {
-	Name        string            `json:"name,omitempty"`
-	Description string            `json:"description,omitempty"`
-	Params      map[string]string `json:"params,omitempty"`
-	Method      string            `json:"method,omitempty"`
+	Name           string            `json:"name,omitempty"`
+	Description    string            `json:"description,omitempty"`
+	Params         map[string]string `json:"params,omitempty"`
+	Method         string            `json:"method,omitempty"`
+	RequiredParams []string          `json:"required_params,omitempty"`
 }
